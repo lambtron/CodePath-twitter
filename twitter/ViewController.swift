62e9fbad6315a317a6fad3e3ffb750ae5fa736ff
Andy Jiang <andyjiang@gmail.com>
2016-10-31 09:31:29 -0700

    Initial Commit

---
+++ twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitter
+//
+//  Created by Andy Jiang on 10/31/16.
+//  Copyright © 2016 andyjiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
