6bc808ef57aa145c337fd5a960dc91294e05fe91
Andy Jiang <andyjiang@gmail.com>
2016-11-01 09:01:04 -0700

    added twitter/tweet.swift

--- twitter/Tweet.swift
+++ twitter/Tweet.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+class Tweet: NSObject {
+    var text: String?
+    var timestamp: Date?
+    var retweetsCount: Int = 0
+    var likesCount: Int = 0
+    
+    init(dictionary: NSDictionary) {
+        text = dictionary["text"] as? String
+        retweetsCount = (dictionary["retweet_count"] as? Int) ?? 0
+        likesCount = (dictionary["favourites_count"] as? Int) ?? 0
+        let timestampString = dictionary["created_at"] as? String
+        
+        if let timestampString = timestampString {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
+            timestamp = formatter.date(from: timestampString)
+        }
+    }
+    
+    class func tweetsWithArray(dictionaries: [NSDictionary]) -> [Tweet] {
+        var tweets = [Tweet]()
+        for dictionary in dictionaries {
+            let tweet = Tweet(dictionary: dictionary)
+            tweets.append(tweet)
+        }
+        return tweets
+    }
+}
