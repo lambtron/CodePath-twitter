2701f18bfd20965a72ca7de716ad72e358e410d3
Andy Jiang <andyjiang@gmail.com>
2016-11-01 09:01:04 -0700

    added twitter/user.swift

--- twitter/User.swift
+++ twitter/User.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+
+class User: NSObject {
+    var name: String?
+    var screenname: String?
+    var profileUrl: URL?
+    var bio: String?
+    var dictionary: NSDictionary?
+    
+    static let userDidLogoutNotification = Notification.Name("UserDidLogout")
+    
+    init(dictionary: NSDictionary) {
+        self.dictionary = dictionary
+        name = dictionary["name"] as? String
+        screenname = dictionary["screen_name"] as? String
+        let profileUrlString = dictionary["profile_immage_url_https"] as? String
+        if let profileUrlString = profileUrlString {
+            profileUrl = URL(string: profileUrlString)
+        }
+        
+        bio = dictionary["description"] as? String
+        
+    }
+    
+    static var _currentUser: User?
+    
+    class var currentUser: User? {
+        get {
+            if User._currentUser == nil {
+                let defaults = UserDefaults.standard
+                let userData = defaults.object(forKey: "currentUserData") as? Data
+                
+                if let userData = userData {
+                    let dictionary = try! JSONSerialization.jsonObject(with: userData, options: []) as! NSDictionary
+                    User._currentUser = User(dictionary: dictionary)
+                }
+            }
+            return _currentUser
+        }
+        set (user) {
+            User._currentUser = user
+            let defaults = UserDefaults.standard
+            if let user = user {
+                let data = try! JSONSerialization.data(withJSONObject: user.dictionary!, options: [])
+                defaults.set(data, forKey: "currentUserData")
+            } else {
+                defaults.removeObject(forKey: "currentUserData")
+            }
+            
+            defaults.synchronize()
+        }
+    }
+}
