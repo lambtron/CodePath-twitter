2f08fedd4594d761097f048c03fe58cd37a9c6fc
Andy Jiang <andyjiang@gmail.com>
2016-11-01 09:01:04 -0700

    added podfile

---
+++ twitter/Tweet.swift
@@ -0,0 +1,9 @@
+//
+//  Tweet.swift
+//  twitter
+//
+//  Created by Andy Jiang on 10/31/16.
+//  Copyright © 2016 andyjiang. All rights reserved.
+//
+
+import Foundation
---
+++ twitter/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  twitter
+//
+//  Created by Andy Jiang on 10/31/16.
+//  Copyright © 2016 andyjiang. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1SessionManager {
+
+}
